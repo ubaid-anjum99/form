bc1b18c74720c80b5e042c717dd7bc18f08c2bf2
ubaidann <anjumu635@gmail.com>
2023-03-09 16:28:49 +0530

    styling

---
+++ dom.js
@@ -0,0 +1,6 @@
+var items=document.getElementsByClassName('list-group-item');
+console.log(items[2]);
+items[2].style.backgroundColor='green';
+for(var i=0;i<=items.length;i++){
+    items[i].style.fontWeight ='bold'
+}
