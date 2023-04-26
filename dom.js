15a58bc08f37adf7a8d34f6345900a29fc0316d1
ubaidann <anjumu635@gmail.com>
2023-04-26 12:22:50 +0530

    font color

--- dom.js
+++ dom.js
@@ -1,6 +1,4 @@
-var items=document.getElementsByClassName('list-group-item');
-console.log(items[2]);
-items[2].style.backgroundColor='green';
-for(var i=0;i<=items.length;i++){
-    items[i].style.fontWeight ='bold'
+let items=document.querySelectorAll(".list-group-item");
+for(let i=0;i<items.length;i+2){
+    items[i].getElementsByClassName.background="green";
 }
